0dbcd163d4acd9e00cc97e5e208652d52e20f412
Nathan Kopp <nkgh@nathankopp.com>
2020-05-02 14:38:18 -0700

    new version

--- linnstrument-firmware.ino
+++ linnstrument-firmware.ino
@@ -57,7 +57,7 @@ For any questions about this, contact Roger Linn Design at support@rogerlinndesi
 /******************************************** CONSTANTS ******************************************/
 
 const char* OSVersion = "222.";
-const char* OSVersionBuild = ".K06";
+const char* OSVersionBuild = ".K07";
 
 // SPI addresses
 #define SPI_LEDS    10               // Arduino pin for LED control over SPI
